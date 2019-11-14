8b367f6a133466948b3d5090d9db86fbf7b2418d
Daniel Davidson <DanDavidson88@gmail.com>
2019-11-14 14:53:40 -0800

    FInal Commit

---
+++ Module8/PartialClassDemo/MyClass.Boilerplate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartialClassDemo
+{
+    partial class MyClass
+    {
+        // static partial class
+        // field and nothing else
+        private int myField;
+
+        // property needs to be public / part of it
+        public int MyProperty { get; set; }
+    }
+}
---
+++ Module8/PartialClassDemo/MyClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartialClassDemo
+{
+    partial class MyClass
+    {
+        // Constructer 
+        public MyClass(int x)
+        {
+            this.myField = x;
+        }
+
+        public void printState()
+        {
+            Console.WriteLine("State: {0}", myField);
+        }
+    }
+}
